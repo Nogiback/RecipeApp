f13a7cfcdecf31cbf8b48f78e08c090bcfba6491
Harlie Tran <anhtran.ngh@gmail.com>
2026-02-22 17:00:23 -0500

    add access button to other entities

---
+++ RecipeApp/ViewModels/CreateIngredientViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecipeApp.ViewModels
+{
+    public class CreateIngredientViewModel
+    {
+        [Required(ErrorMessage = "Ingredient name is required")]
+        [Display(Name = "Ingredient Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; set; }
+
+        [Required(ErrorMessage = "Default unit is required")]
+        [Display(Name = "Default Unit (e.g., g, ml, pcs)")]
+        public string DefaultUnit { get; set; }
+
+        [Required(ErrorMessage = "Please select a category")]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        // This fulfills the dropdown list requirement 
+        public IEnumerable<SelectListItem> CategoryOptions { get; set; }
+    }
+}
